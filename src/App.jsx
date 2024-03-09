af04f21bc5f47fbdf7d53cb65c4850a0994058b4
PessahDerek <pesaderek1738@gmail.com>
2024-03-10 02:12:10 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,19 @@ import "./App.css";
 
 import Entries from "./pages/Entries";
 import Totals from "./pages/Totals";
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
  
-    <Router>
+    // <Router>
       <div className="App">
         <Routes>
           <Route path="/" element={<Entries />} />
           <Route path="/totals" element={<Totals />} />
         </Routes>
       </div>
-    </Router>
+    // </Router>
   );
 }
 
