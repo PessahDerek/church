2711a19f60f34d9f26a7ad5c41f0e09d50ccd615
PessahDerek <pesaderek1738@gmail.com>
2024-03-10 02:14:58 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
-
 import "./App.css";
-
 import Entries from "./pages/Entries";
 import Totals from "./pages/Totals";
-import { Route, Routes } from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 
 function App() {
-  return (
- 
-    // <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Entries />} />
-          <Route path="/totals" element={<Totals />} />
-        </Routes>
-      </div>
-    // </Router>
-  );
+    return (
+
+        <div className="App">
+            <Routes>
+                <Route index={true} element={<Entries/>}/>
+                <Route path={"/totals"} element={<Totals/>}/>
+                <Route path={"/*"} element={<div>
+                    <h1>Man Wtf</h1>
+                </div>} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
